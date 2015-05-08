cad30a2a60d32b3a654c0c7707b2514383911204
yangbin <18774625353@139.com>
2015-05-08 10:32:02 +0800

    Initial Commit

---
+++ YBImageShow/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YBImageShow
+//
+//  Created by 杨彬 on 15/5/8.
+//  Copyright (c) 2015年 macbook air. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YBImageShow/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YBImageShow
+//
+//  Created by 杨彬 on 15/5/8.
+//  Copyright (c) 2015年 macbook air. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
